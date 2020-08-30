af98c336ff971ea97760d9abdc84301fdef9dbbf
comet-602 <connecting941@gmail.com>
2020-08-30 16:15:24 +0800

    update rf

--- balance_sheet.py
+++ balance_sheet.py
@@ -32,7 +32,7 @@ def tot(year,season):
 
 
     # 新增日期並調整欄位位置
-    df['year_season']=str(year)+str(season)
+    df['date']=str(year)+str(season)
     cols = df.columns.values.tolist()
     cols = cols[-1:] + cols[:-1]
     df=df[cols]
@@ -40,11 +40,11 @@ def tot(year,season):
     # 存入CSV
     df.to_csv('data/balance_sheet.csv',mode='a',encoding='utf-8-sig', index=0, header=0)
 
-    return print('筆數:',len(df['year_season']))
+    return print('筆數:',len(df['date']))
 
 
 # 建立欄位名稱並存入CSV
-columns=['year_season','公司代號', '公司名稱', '流動資產', '非流動資產', '資產總額', '流動負債', '非流動負債', '負債總額', '股本', '資本公積', '保留盈餘', '其他權益', '庫藏股票', '歸屬於母公司業主之權益合計', '共同控制下前手權益',
+columns=['date','公司代號', '公司名稱', '流動資產', '非流動資產', '資產總額', '流動負債', '非流動負債', '負債總額', '股本', '資本公積', '保留盈餘', '其他權益', '庫藏股票', '歸屬於母公司業主之權益合計', '共同控制下前手權益',
  '合併前非屬共同控制股權', '非控制權益', '權益總額', '預收股款（權益項下）之約當發行股數（單位：股）', '母公司暨子公司所持有之母公司庫藏股股數（單位：股）', '每股參考淨值']
 
 df=pd.DataFrame(columns=columns)
--- random_forest.py
+++ random_forest.py
@@ -3,18 +3,27 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score,accuracy_score,roc_auc_score
 
-df = pd.read_csv('./data/income_after_pearson.csv')
-print(df.info())
-train_data = df[df['date']<1074]
-test_data = df[df['date']>=1074]
+def rf(path):
+    df = pd.read_csv(path)
+    #print(df.info())
+    train_data = df[df['date']<1074]
+    test_data = df[df['date']>=1074]
 
-X_train = train_data.loc[:,"營業收入":"綜合損益總額歸屬於共同控制下前手權益"].values
-y_train = train_data['label'].values
-X_test = test_data.loc[:,"營業收入":"綜合損益總額歸屬於共同控制下前手權益"].values
-y_test = test_data['label'].values
+    X_train = train_data.iloc[:,3:-2].values
+    y_train = train_data['label'].values
+    X_test = test_data.iloc[:,3:-2].values
+    y_test = test_data['label'].values
 
-rnd_clf = RandomForestClassifier(n_estimators=500, max_leaf_nodes=16, random_state=42)
-rnd_clf.fit(X_train, y_train)
+    rnd_clf = RandomForestClassifier(n_estimators=500, max_leaf_nodes=16, random_state=42)
+    rnd_clf.fit(X_train, y_train)
 
-print('train score:',accuracy_score(y_train,rnd_clf.predict(X_train)))
-print('test score:',accuracy_score(y_test,rnd_clf.predict(X_test)))
+    print('train score:',accuracy_score(y_train,rnd_clf.predict(X_train)))
+    print('test score:',accuracy_score(y_test,rnd_clf.predict(X_test)))
+    return
+
+rf('./data/income_sheet_clean_after_pearson.csv')
+print('========================')
+rf('./data/balance_sheet_clean_after_pearson.csv')
+
+# df = pd.read_csv('./data/income_sheet_clean_after_pearson.csv')
+# print(df.iloc[:,3:-2])
